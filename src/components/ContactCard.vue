e0ec2dbabd91e8cddaea0e51b78c6fafdac38550
Martel-359 <doanhuuvinh0108@gmail.com>
2023-08-24 09:01:27 +0700

    Hien thi danh sach contact lay ve tu server

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="p-1 d-inline-block">
+      <strong>Tên:</strong>
+      {{ contact.name }}
+    </div>
+
+    <div class="p-1 d-inline-block">
+      <strong>E-mail:</strong>
+      {{ contact.email }}
+    </div>
+    <div class="p-1 d-inline-block">
+      <strong>Địa chỉ:</strong>
+      {{ contact.address }}
+    </div>
+    <div class="p-1 d-inline-block">
+      <strong>Điện thoại:</strong>
+      {{ contact.phone }}
+    </div>
+    <div class="p-1 d-inline-block">
+      <strong>Liên hệ yêu thích:&nbsp;</strong>
+      <i v-if="contact.favorite" class="fa fa-check"></i>
+      <i v-else class="fa fa-times"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    contact: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
